169a6f6d71357c8a564c13ad606672fd1e4eceac
Benjamin Lefaudeux <benjamin.lefaudeux@gmail.com>
2014-01-06 15:57:17 +0100

    Nothing fancy

--- Camera calibration/batch_calibrate.py
+++ Camera calibration/batch_calibrate.py
@@ -21,7 +21,6 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 """
 
 import camera_calibration as cam_calib
