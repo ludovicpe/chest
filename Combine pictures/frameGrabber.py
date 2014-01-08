63f2d20c64ca6604d932ed086cd58609c5666c56
Benjamin Lefaudeux <benjamin.lefaudeux@gmail.com>
2014-01-08 13:20:20 +0100

    a few bugfixes

--- Combine pictures/frameGrabber.py
+++ Combine pictures/frameGrabber.py
@@ -12,75 +12,76 @@ import os
 import utils as ut
 
 """
-  The overall frameGrabber class, from which all our sub-classes inherit
+  The overall FrameGrabber class, from which all our sub-classes inherit
 """
-class frameGrabber:  
+class FrameGrabber:
   n_frame       = 0
   n_max_frames  = 0
   size_x        = 0
-  size_y        = 0  
-  
+  size_y        = 0
+
   def __init__(self):
     # The default constructor
     self.n_frames     = 0
     self.n_max_frames = 0
     self.size_x       = 0
-    self.size_y       = 0  
-    
+    self.size_y       = 0
+
     return 'New empty frame grabber'
 
-  def showPictInWindow(self, pict):
+  @staticmethod
+  def show_pict_window(pict):
     b_quit = False
-    
+
     cv2.namedWindow("Show")
-    cv2.imshow("Show", pict)  
+    cv2.imshow("Show", pict)
     k = cv2.waitKey(33)
     # Escape quits
-    if (k==27):
+    if k==27:
         b_quit = True
         cv2.destroyAllWindows()
-        
+
     return b_quit
-    
+
   def release(self):
     pass
-  
-  
+
+
 """
 Inherited class : read a video file
-"""  
-class videoFile(frameGrabber):
+"""
+class VideoFile(FrameGrabber):
   # The constructor : get a handle on a video file
   def __init__(self, filename):
     self.n_frames   = 0
     self.capture    = cv2.VideoCapture(filename)
     self.keep_going = True
-    
+
   # Get a new frame
   def newFrame(self):
-    if (self.keep_going):  
-      self.keep_going, self.frame = self.capture.read()    
-      self.n_frame = self.n_frame + 1  
+    if (self.keep_going):
+      self.keep_going, self.frame = self.capture.read()
+      self.n_frame = self.n_frame + 1
       return [True, self.frame]
-  
+
     else :
       return [False, []]
-  
+
   def show(self):
-    frameGrabber.showPictInWindow(self.frame)
-  
-  
+    FrameGrabber.show_pict_window(self.frame)
+
+
 """
 Inherited class : read from a connected cam
-"""  
-class webCam(frameGrabber):
+"""
+class Webcam(FrameGrabber):
   def __init__(self, device_id = 0):
     self.n_frames = 0
     self.cam = cv2.VideoCapture(device_id)
-    
+
     if self.cam.isOpened():
       self.cam.release()
-      
+
     self.cam.open(device_id)
     self.keep_going = True
 
@@ -90,73 +91,73 @@ class webCam(frameGrabber):
 
     if self.keep_going :
       return [True, self.frame_last]
-      
+
     else:
       self.cam.release()
-      return [False, []]      
-      
+      return [False, []]
+
   def show(self):
     keep_going = True
-    
+
     while keep_going:
       grab_success, self.frame = self.newFrame()
-      
+
       if not grab_success:
         return
-        
+
       else :
-        b_quit = frameGrabber.showPictInWindow(self, self.frame)
+        b_quit = FrameGrabber.show_pict_window(self, self.frame)
         keep_going = keep_going and not b_quit
-        
+
   # Re-implement this method
   # in this case we really want to release the connected cam
   def release(self):
     self.cam.release()
-      
-      
+
+
 """
 Inherited class : read a sequence of pictures
-"""  
-class pictsFile(frameGrabber):
-  # The constructor : get a list of all the frames, and the number of frames 
+"""
+class PictsFile(FrameGrabber):
+  # The constructor : get a list of all the frames, and the number of frames
   def __init__(self, folder):
-    [self.pict_list, self.n_max_frames] = self.getPictFiles(folder)    
-    self.n_frames = 0    
-    
+    [self.pict_list, self.n_max_frames] = self.getPictFiles(folder)
+    self.n_frames = 0
+
   def newFrame(self):
     if self.n_frames < (self.n_max_frames-1):
-      self.n_frames = self.n_frames + 1
+      self.n_frames += 1
       return [True, self.pict_list[self.n_frames]]
-    
-    else : 
+
+    else :
       return [False, []]
-    
+
   def getPictFiles(self, folder):
       """
       Get all the picture files in the folder
       """
-  
+
       folder_path = os.path.abspath(folder)
-  
+
       if not os.path.isdir(folder_path):
           print "Trouble reading folder path {}".format(folder_path)
-  
+
       picture_list = []
       n_files = 0
-  
+
       for dirname, dirnames, filenames in os.walk(folder_path):
-  
+
           filenames = ut.sort_nicely(filenames)
-  
+
           for filename in filenames:
               print "Reading file {}".format(filename)
-  
+
               full_filepath = os.path.join(folder_path, filename)
-  
+
               if (filename[-3:] == "bmp") or \
                   (filename[-3:] == "png") or \
                   (filename[-3:] == "jpg") :
-  
+
                   try:
                       picture_list.append(cv2.imread(full_filepath, cv2.CV_LOAD_IMAGE_GRAYSCALE))
                       if (picture_list[n_files] == None) :
@@ -164,8 +165,8 @@ class pictsFile(frameGrabber):
                           print "Error loading file {}".format(filename)
                       else :
                           n_files = n_files + 1
-  
+
                   except:
                       print "Error loading file {}".format(filename)
-  
+
       return picture_list, n_files
